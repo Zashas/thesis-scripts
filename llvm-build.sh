c575777b3ebad4da1fb7a87bd3b2a5ce497eed74
Mathieu Xhonneux <m.xhonneux@gmail.com>
2017-11-13 17:16:15 +0000

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -1,3 +1,7 @@
+# Follow instructions https://clang.llvm.org/get_started.html
+
+# replace cmake in step 7 by :
+
 cmake -G 'Unix Makefiles' \
         -DLLVM_TARGETS_TO_BUILD:STRING="BPF" \
         -DCMAKE_BUILD_TYPE:STRING=Release \
